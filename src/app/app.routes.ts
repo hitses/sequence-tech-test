fc76074bda6f0938841dab7a787b4882f9520ef9
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-06 11:42:43 +0100

    Agregadas rutas para la página de canciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'songs', loadChildren: () => import('./songs/songs.routes') },
+  { path: '**', redirectTo: 'songs' },
+];
