0a098f0dd26b6eb9544626b3c6d35a4e1b9b2de1
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-06 11:40:18 +0100

    Eliminar pruebas innecesarias de AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'sequence-tech-test' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sequence-tech-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sequence-tech-test');
-  });
 });
