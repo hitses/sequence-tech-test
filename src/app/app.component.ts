be98f2669045a9d4a120b2d6b606c6f979ae6ff1
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-06 10:52:51 +0100

    Añadir soporte para traducción en el componente AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { defaultLanguage, languages } from '@config/languages';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'sequence-tech-test';
+  translate = inject(TranslateService);
+
+  languages = signal<string[]>(languages);
+  defaultLanguage = signal<string>(defaultLanguage);
+  translation = signal(
+    this.translate.getBrowserLang() || this.defaultLanguage()
+  );
+
+  ngOnInit(): void {
+    this.translate.addLangs(this.languages());
+    this.translate.use(this.translation());
+  }
 }
