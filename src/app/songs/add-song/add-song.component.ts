c6b4c5d412f799cf1a7a004daa687558864fdba7
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-06 11:41:27 +0100

    Agregado componente crear canción

---
+++ src/app/songs/add-song/add-song.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-song',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-song.component.html',
+  styleUrl: './add-song.component.scss',
+})
+export default class AddSongComponent {}
