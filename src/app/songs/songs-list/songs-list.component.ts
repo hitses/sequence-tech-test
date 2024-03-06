0a40bd5399d1fcaed3fefae0fdc925d483485927
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-06 11:40:49 +0100

    Agregado componente de lista de canciones

---
+++ src/app/songs/songs-list/songs-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-songs',
+  standalone: true,
+  imports: [],
+  templateUrl: './songs-list.component.html',
+  styleUrl: './songs-list.component.scss',
+})
+export default class SongsListComponent {}
