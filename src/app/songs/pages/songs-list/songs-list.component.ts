3b08d3ee376c0cae7ed623e3f65a88c8615ca5fa
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-07 07:52:44 +0100

    Mostrar la lista de canciones

--- src/app/songs/pages/songs-list/songs-list.component.ts
+++ src/app/songs/pages/songs-list/songs-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
+import { SongCardComponent } from '@app/songs/components/song-card/song-card.component';
+
+import { SongsService } from '@app/songs/songs.service';
 
 @Component({
   selector: 'app-songs',
   standalone: true,
-  imports: [],
+  imports: [SongCardComponent],
   templateUrl: './songs-list.component.html',
   styleUrl: './songs-list.component.scss',
 })
-export default class SongsListComponent {}
+export default class SongsListComponent {
+  private readonly songsService = inject(SongsService);
+
+  songs = this.songsService.songs;
+}
