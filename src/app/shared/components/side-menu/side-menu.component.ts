66bd566e1e3db94a357f01024d59021e07fee618
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 12:22:33 +0100

    Actualización del menú lateral

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SideMenuService } from '@app/shared/side-menu.service';
 
 @Component({
   selector: 'side-menu-component',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  readonly sideMenuService = inject(SideMenuService);
+
+  closeMenu() {
+    this.sideMenuService.closeMenu();
+  }
+}
