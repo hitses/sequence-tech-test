9341f8a7d20061a7015d14a2e3bd180c975ff722
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 23:12:58 +0100

    Añadir botón, funcionalidad, modal y traducciones de eliminación de canción

--- src/app/songs/pages/song/song.component.ts
+++ src/app/songs/pages/song/song.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { SongsService } from '../../../shared/services/songs.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { HeaderComponent } from '@app/shared/components/header/header.component';
 import { HeaderService } from '@app/shared/services/header.service';
@@ -9,7 +9,9 @@ import { ArtistsService } from '@app/shared/services/artists.service';
 import { Artist } from '@app/shared/models/artist.interface';
 import { CompaniesService } from '@app/shared/services/companies.service';
 import { Company } from '@app/shared/models/company.interface';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-song',
@@ -19,11 +21,14 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './song.component.scss',
 })
 export default class SongComponent {
-  private route = inject(ActivatedRoute);
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
   private readonly songsService = inject(SongsService);
   private readonly artistsService = inject(ArtistsService);
   private readonly companiesService = inject(CompaniesService);
   private readonly headerService = inject(HeaderService);
+  private readonly toastr = inject(ToastrService);
+  private translate = inject(TranslateService);
 
   song = this.songsService.song;
   artist = this.artistsService.artist;
@@ -54,5 +59,45 @@ export default class SongComponent {
     const remainingSeconds: number = seconds % 60;
 
     return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  }
+
+  deleteSong() {
+    this.translate
+      .get(
+        [
+          'songs.song.delete.title',
+          'songs.song.delete.text',
+          'songs.song.delete.confirm',
+          'songs.song.delete.cancel',
+        ],
+        { value: this.song().title }
+      )
+      .subscribe((translations) => {
+        Swal.fire({
+          title: translations['songs.song.delete.title'],
+          text: translations['songs.song.delete.text'],
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: translations['songs.song.delete.confirm'],
+          cancelButtonText: translations['songs.song.delete.cancel'],
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.songsService.deleteSongById(this.song().id).subscribe({
+              next: () => {
+                this.toastr.info(
+                  'Canción eliminada',
+                  `${this.song().title} eliminada de tu biblioteca`
+                );
+                this.router.navigate(['songs']);
+              },
+              error: (error) => {
+                console.error(error);
+              },
+            });
+          }
+        });
+      });
   }
 }
