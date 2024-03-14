f21671a0d39ba5e9f473c1566739171c0460781f
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 11:14:00 +0100

    Añadir encabezado y traducir texto en el componente de lista de canciones

--- src/app/songs/pages/songs-list/songs-list.component.ts
+++ src/app/songs/pages/songs-list/songs-list.component.ts
@@ -5,20 +5,31 @@ import { SongCardComponent } from '@app/songs/components/song-card/song-card.com
 import { SongsService } from '@app/shared/songs.service';
 import { ArtistsService } from '@app/shared/artists.service';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '@app/shared/components/header/header.component';
+import { HeaderService } from '@app/shared/header.service';
 
 @Component({
   selector: 'app-songs',
   standalone: true,
-  imports: [SongCardComponent, RouterLink],
+  imports: [SongCardComponent, RouterLink, HeaderComponent],
   templateUrl: './songs-list.component.html',
   styleUrl: './songs-list.component.scss',
 })
 export default class SongsListComponent {
   private readonly songsService = inject(SongsService);
   private readonly artistsService = inject(ArtistsService);
+  private readonly headerService = inject(HeaderService);
 
   songs = this.songsService.songs;
   artists = this.artistsService.artists;
+
+  ngOnInit() {
+    this.headerService.headerAction.subscribe((action) => {
+      if (action === 'menu') {
+        console.log('Abro el menú');
+      }
+    });
+  }
 
   getArtistName(id: number): string {
     const artist = this.artists().find((artist) => artist.id === id);
