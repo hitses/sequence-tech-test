ddb09c8cf85869eff82562bf2b21d025995e10f2
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 12:12:04 +0100

    Agregado componente de menú lateral

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-menu-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss',
+})
+export class SideMenuComponent {}
