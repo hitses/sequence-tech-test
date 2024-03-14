7d66b9e427def2e38d4f254be4ae4b0654bf1842
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 13:37:54 +0100

    Mostrar detalles de canción en Song component

--- src/app/songs/pages/song/song.component.ts
+++ src/app/songs/pages/song/song.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SongsService } from '../../../shared/services/songs.service';
+import { JsonPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-song',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './song.component.html',
   styleUrl: './song.component.scss',
 })
-export default class SongComponent {}
+export default class SongComponent {
+  private route = inject(ActivatedRoute);
+  private readonly songsService = inject(SongsService);
+
+  song = this.songsService.song;
+
+  constructor() {
+    this.route.params
+      .pipe(switchMap(async ({ id }) => this.songsService.getSongById(id)))
+      .subscribe();
+  }
+}
