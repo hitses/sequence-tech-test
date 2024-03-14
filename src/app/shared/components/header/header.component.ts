77399f0bfb110e57733685860ef3ea8847da4a5d
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 16:54:32 +0100

    Agrega importaciones y redirección en el componente de encabezado

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, input } from '@angular/core';
+import { Router } from '@angular/router';
 import { HeaderService } from '@app/shared/services/header.service';
+import { SongsService } from '@app/shared/services/songs.service';
 
 @Component({
   selector: 'header-component',
@@ -9,6 +11,8 @@ import { HeaderService } from '@app/shared/services/header.service';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private readonly router = inject(Router);
+  private readonly songsService = inject(SongsService);
   private readonly headerService = inject(HeaderService);
 
   icon = input.required<string>();
@@ -17,8 +21,12 @@ export class HeaderComponent {
   onClickIcon() {
     if (this.title() === 'Canciones') {
       this.headerService.performAction('menu');
-    } else if (this.title() === 'Nueva canción') {
-      this.headerService.performAction('back');
+    } else if (
+      this.title() === this.songsService.song().title ||
+      this.title() === 'Nueva canción'
+    ) {
+      this.headerService.performAction('');
+      this.router.navigate(['songs']);
     }
   }
 }
