40be4021378e0ff1bd6e7232ac813216433f5ecb
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2024-03-14 22:33:29 +0100

    Añadidas traducciones a SideMenuComponent

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { SideMenuService } from '@app/shared/services/side-menu.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'side-menu-component',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss',
 })
